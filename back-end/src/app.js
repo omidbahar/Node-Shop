6b57bebdab26a7acbaeb85f8307ce24acbf1b753
omidbahar <omidbahar@gmail.com>
2019-07-07 01:06:43 +0400

    installation done for back-end and front-end

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,5 +1,14 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser'); //for using JSON data very easily
+const cors = require('cors'); //CORS allows you to configure the web API's security.
+const morgan = require('morgan'); //log generator
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+
 app.get('/', (req, res)=>{
     res.send(`<h1>Hello</h1>`);
 
